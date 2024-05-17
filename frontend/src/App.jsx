aa4931b1e84d874f54833451537de664cfe5ab4b
k3vin333 <k3vinalt@gmail.com>
2024-05-17 13:37:36 +1000

    added styling for form pass and user input, login and background, created browser routed with Outlet and children

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,68 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom"
+
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Write from "./pages/Write"
+import Home from "./pages/Homepage"
+import Single from "./pages/Single"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import "./style.scss"
+
+const Layout = () => {
+  return (
+  <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+  </>
+  );
+;}
+
+// Router path is pretty much the URL for pages
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/post/:id",
+        element:<Single/>
+      },
+      {
+        path:"/write",
+        element:<Write/>
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router}/>
+      </div>
+    </div>
+    );
 }
+
 
 export default App;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='auth'>
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder='username' />
+        <input type="password" placeholder='password' />
+        <button>Login</button>
+        <p>Error!</p>
+        <span>Don't have an account? <Link to="/register">Register</Link></span>
+      </form>
+    </div>
   )
 }
 
