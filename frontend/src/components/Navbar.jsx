f7d0698191e4ffa3a5402401b9c40a7761beff2b
k3vin333 <k3vinalt@gmail.com>
2024-05-23 01:44:35 +1000

    added links to pages, added logo (temp)

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from "../img/Logo.jpg"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="container">
+        <div className="logo">
+            <img src={Logo} alt="" />
+        </div>
+        <div className="links">
+            <Link className='link' to ="/?cat=programming">
+                <h6>PROGRAMMING</h6>
+            </Link>
+            <Link className='link' to ="/?cat=cooking">
+                <h6>COOKING</h6>
+            </Link>
+            <Link className='link' to ="/?cat=hobbies">
+                <h6>HOBBIES</h6>
+            </Link>
+            <Link className='link' to ="/?cat=portfolio">
+                <h6>PORTFOLIO</h6>
+            </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='auth'>
+      <h1>Login</h1>
+      <form>
+        <input required type="text" placeholder='username' />
+        <input required type="email" placeholder='email' />
+        <input required type="password" placeholder='password' />
+        <button>Login</button>
+        <p>Error!</p>
+        <span>Do you have an account? <Link to="/login">Register</Link></span>
+        <span> <Link to="/">Homepage</Link></span>
+      </form>
+    </div>
   )
 }
 
